2252b71d9437f0544882023c5820b6dc827036fc
MartinKrutil <martin.krutil@email.cz>
2024-05-22 02:36:30 +0200

    test huffman encoding

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include "huffman/huffman.h"
 
 int main() {
+    char values[] = {};
+    int frequencies[] = {};
+    int size = sizeof(values) / sizeof(values[0]);
+    
+    huffmanEncode(values, frequencies, size);
 
+    return 0;
 }
