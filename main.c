f1da7f0602a81308c2b7a2ba5a7504954aa0350e
MartinKrutil <martin.krutil@email.cz>
2024-05-24 01:25:38 +0200

    test shannon-fano encoding

--- main.c
+++ main.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
-#include "huffman/huffman.h"
+#include <stdlib.h>
+#include <string.h>
+// #include "huffman/huffman.h"
+#include "shannon-fano/shannon_fano.h"
 
 int main() {
-    char values[] = {'c', 'b', 'a', 'e', 'd', 'f'};
-    int frequencies[] = {12, 9, 5, 16, 13, 45};
-    int size = sizeof(values) / sizeof(values[0]);
+    char values[] = "cbaedf";
+    float frequencies[] = {0.12, 0.09, 0.05, 0.16, 0.13, 0.45};
     
-    huffmanEncode(values, frequencies, size);
+    SFNode *node = newSFNode(values, frequencies[0]);
+    SFNode *node2 = newSFNode(NULL, frequencies[0]);
+
+    SFNode *tree = buildShannonFanoTree(values, frequencies);
+
+    if (node) {
+        printf("value: %s | frequency: %.2f\n", node->value, node->frequency);
+    }
+    
+    // huffmanEncode(values, frequencies, strlen(values));
+    // qsort();
 
     return 0;
 }
