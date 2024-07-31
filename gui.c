1a7b661699b389b3f4c76750190ac33a97ae4e96
Martin Krutil <martin.krutil@email.cz>
2024-07-31 02:22:52 +0200

    adding gui

--- gui.c
+++ gui.c
@@ -1,9 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "huffman/huffman.h"
-#include "shannon-fano/shannon_fano.h"
-
 #ifndef RAYGUI_IMPLEMENTATION
 #define RAYGUI_IMPLEMENTATION
 
@@ -45,36 +39,3 @@ int gui()
 }
 
 #endif //RAYGUI_IMPLEMENTATION
-
-int main() {
-    // char values[] = "cbaedf";
-    // float frequencies[] = {12, 9, 5, 16, 13, 45};
-
-    char values[] = "abcde";
-    float frequencies[] = {0.22, 0.28, 0.15, 0.30, 0.05};
-
-    printf("values: ");
-    for (size_t i = 0; i < strlen(values); i++) {
-        printf("%c, ", values[i]);
-    }
-    printf("\n");
-
-    printf("frequencies: ");
-    for (size_t i = 0; i < sizeof(frequencies) / sizeof(float); i++) {
-        printf("%.2f, ", frequencies[i]);
-    }
-    printf("\n\n");
-
-    printf("huffman:\n");
-    huffmanEncode(values, frequencies, strlen(values));
-    
-    printf("\nshannon fano:\n");
-    SFNode *tree = shannonFanoEncode(values, frequencies);
-    
-    uint8_t codeBuffer[256];
-    printSFTree(tree, 0, codeBuffer);
-
-    gui();
-
-    return 0;
-}
